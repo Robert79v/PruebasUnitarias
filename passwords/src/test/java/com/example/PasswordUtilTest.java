ae5c8b40e64b20eacf8f4da4a72be4319b350185
Cristian Castañeda <lalo212003@gmail.com>
2024-10-13 19:32:12 -0600

    Test completed

--- passwords/src/test/java/com/example/PasswordUtilTest.java
+++ passwords/src/test/java/com/example/PasswordUtilTest.java
@@ -1,20 +1,26 @@
 package com.example;
 
-import static org.junit.Assert.*;
-import static com.example.PasswordUtil.SecurityLevel.*;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import static com.example.PasswordUtil.SecurityLevel.MEDIUM;
+import static com.example.PasswordUtil.SecurityLevel.STRONG;
+import static com.example.PasswordUtil.SecurityLevel.WEAK;
+
 public class PasswordUtilTest {
+    // Prueba para verificar que una contraseña vacía sea clasificada como WEAK.
     @Test
     public void weak_when_password_is_empty() {
         assertEquals(WEAK, PasswordUtil.assessPassword(""));
     }
     
+    //Prueba para verificar que una contraseña con menos de 8 caracteres sea clasificada como WEAK.
     @Test
     public void weak_when_has_less_than_8_letters() {
         assertEquals(WEAK, PasswordUtil.assessPassword("1234567"));
     }
     
+    //Prueba para verificar que una contraseña que contiene solo letras sea clasificada como WEAK.
     @Test
     public void weak_when_has_only_letters() {
         assertEquals(WEAK, PasswordUtil.assessPassword("abcdefg"));
@@ -29,4 +35,4 @@ public class PasswordUtilTest {
     public void strong_when_has_letters_numbers_and_special_chars() {
         assertEquals(STRONG, PasswordUtil.assessPassword("abc12345!@#"));
     }
-    }
+}
